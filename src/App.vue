b88fdff9cac07f414cb421c0e9121a58824bb3d4
leandrokaminise <leandrotetsuo98@gmail.com>
2024-02-08 18:15:53 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+  <template>
+    <div id="app">
+      <h1>Calculadora Aritmética</h1>
+      
+      <div>
+        <label for="num1">Número 1:</label>
+        <input type="number" v-model="num1" @input="calcularResultado" />
+      </div>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+      <div>
+        <label for="num2">Número 2:</label>
+        <input type="number" v-model="num2" @input="calcularResultado" />
+      </div>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+      <div>
+        <label for="operacao">Operação:</label>
+        <select v-model="operacao" @change="calcularResultado">
+          <option value="+">Soma</option>
+          <option value="-">Subtração</option>
+          <option value="*">Multiplicação</option>
+          <option value="/">Divisão</option>
+        </select>
+      </div>
+
+      <div>
+        <label for="resultado">Resultado:</label>
+        <input type="text" :value="resultado" readonly />
+      </div>
+    </div>
+  </template>
+
+  <script>
+  export default {
+    data() {
+      return {
+        num1: 0,
+        num2: 0,
+        operacao: '+',
+        resultado: 0
+      };
+    },
+    methods: {
+      calcularResultado() {
+        switch (this.operacao) {
+          case '+':
+            this.resultado = this.num1 + this.num2;
+            break;
+          case '-':
+            this.resultado = this.num1 - this.num2;
+            break;
+          case '*':
+            this.resultado = this.num1 * this.num2;
+            break;
+          case '/':
+            this.resultado = this.num1 / this.num2;
+            break;
+        }
+      }
+    }
+  };
+  </script>
+
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
   }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  div {
+    margin-bottom: 10px;
+  }
+
+  label {
+    margin-right: 10px;
+  }
+  </style>
